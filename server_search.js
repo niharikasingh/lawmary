b2ac506101fe477c93f6bf8b2caade8169490372
Niharika Singh <s.niarika09@gmail.com>
2016-09-05 21:53:01 -0400

    setting up kue

--- server_search.js
+++ server_search.js
@@ -15,7 +15,7 @@ db.on( 'error', console.error.bind( console, 'connection error:' ) );
 // INITIALIZE KUE
 
 kue.redis.createClient = function() {
-    var redisUrl = url.parse(process.env.REDIS_URL)
+    var redisUrl = url.parse(process.env.REDIS_URL || "redis://h:p6baq7eutej9n043d5cdg1vv8nm@ec2-54-163-236-235.compute-1.amazonaws.com:22159")
       , client = redis.createClient(redisUrl.port, redisUrl.hostname);
     if (redisUrl.auth) {
         client.auth(redisUrl.auth.split(":")[1]);
--- server_summarize.js
+++ server_summarize.js
@@ -4,7 +4,7 @@ var pythonShell = require('python-shell');
 var url = require('url');
 
 kue.redis.createClient = function() {
-    var redisUrl = url.parse(process.env.REDIS_URL)
+    var redisUrl = url.parse(process.env.REDIS_URL || "redis://h:p6baq7eutej9n043d5cdg1vv8nm@ec2-54-163-236-235.compute-1.amazonaws.com:22159")
       , client = redis.createClient(redisUrl.port, redisUrl.hostname);
     if (redisUrl.auth) {
         client.auth(redisUrl.auth.split(":")[1]);
